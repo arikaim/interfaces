392bb6bc662e0469f80a2468838ddac548ebc8fa
Arikaim <sales@intersoft.biz>
2020-03-13 10:35:03 +0200

    =

--- RoutesInterface.php
+++ RoutesInterface.php
@@ -14,5 +14,63 @@ namespace Arikaim\Core\Interfaces;
  */
 interface RoutesInterface
 {    
-    
+    /**
+     * Save route redirect url
+     *
+     * @param string $method
+     * @param string $pattern
+     * @param string $url
+     * @return boolean
+     */
+    public function setRedirectUrl($method, $pattern, $url);
+
+    /**
+     * Set routes status
+     *
+     * @param array     $filterfilter
+     * @param integer   $status
+     * @return boolean
+     */
+    public function setRoutesStatus($filter = [], $status);
+
+    /**
+     * Add api route
+     *
+     * @param string $method
+     * @param string $pattern
+     * @param string $handlerClass
+     * @param string $handlerMethod
+     * @param string $extension
+     * @param integer|null $auth
+     * @return bool
+     */
+    public function addApiRoute($method, $pattern, $handlerClass, $handlerMethod, $extension, $auth = null);
+
+    /**
+     * Return true if reoute exists
+     *
+     * @param string $method
+     * @param string $pattern
+     * @return boolean
+     */
+    public function has($method, $pattern);
+
+    /**
+     * Delete route
+     *
+     * @param string $method
+     * @param string $pattern
+     * @return bool
+     */
+    public function delete($method, $pattern);
+
+    /**
+     * Save route options
+     *
+     * @param string $method
+     * @param string $pattern
+     * @param array $options
+     * @return boolean
+     */
+    public function saveRouteOptions($method, $pattern, $options);
 }
